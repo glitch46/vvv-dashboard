e00c03b34e42cff546fa8b274483eb29479e5b63
glitch46 <cminarcin711@gmail.com>
2026-02-23 17:51:51 -0600

    fix: use liquid VVV for DEX volume; clarify labels and units

--- scripts/refresh.py
+++ scripts/refresh.py
@@ -5,6 +5,7 @@ API_KEY = os.environ['DUNE_API_KEY']
 CG_KEY = os.environ.get('COINGECKO_API_KEY')
 headers = {'X-DUNE-API-KEY': API_KEY, 'Content-Type': 'application/json'}
 svvv = '0x321b7ff75154472B18EDb199033fF4D116F340Ff'
+vvv = '0x7F69F77D99Ee63d97c76b1E0F19dFf7D477C4E4f'  # liquid VVV for DEX volume
 method = '0xae5ac921'
 
 sql1 = f"""
@@ -167,7 +168,7 @@ try:
            SUM(amount_usd) AS trade_volume_usd
     FROM dex.trades
     WHERE blockchain='base'
-      AND (token_bought_address = {svvv} OR token_sold_address = {svvv})
+      AND (token_bought_address = {vvv} OR token_sold_address = {vvv})
       AND block_time >= now() - interval '30' day
     GROUP BY 1
     """
